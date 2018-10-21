2931067825ef8830308e39541da0c6d6dbc77139
İrem Büşra KILIÇ <37072323+irembusra@users.noreply.github.com>
2018-10-21 23:26:02 +0300

    Dragon Movement
    Taking damage system

---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour {
+
+    public float speed;
+    Animator anim;
+    public int dir;
+    float timer = .7f;
+    public int health;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        dir = Random.Range(0, 3);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            timer = .7f;
+            dir = Random.Range(0, 3);
+
+        }
+        Movement();
+	}
+    void Movement()
+    {
+        switch (dir)
+        {
+            case 0:
+                transform.Translate(0, speed * Time.deltaTime, 0);
+                anim.SetInteger("dir", dir);
+                break;
+            case 1:
+                transform.Translate(-speed * Time.deltaTime,0, 0);
+                anim.SetInteger("dir", dir);
+                break;
+            case 2:
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+                anim.SetInteger("dir", dir);
+                break;
+            case 3:
+                transform.Translate(speed * Time.deltaTime,0, 0);
+                anim.SetInteger("dir", dir);
+                break;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Sword")
+        {
+            health--;
+            collision.gameObject.GetComponent<Sword>().CreateParticle();
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().canAttack = true;
+            Destroy(collision.gameObject);
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+       
+    }
+}
